5ba1fa66afdb4a4fe66f20c13c084aec1c496733
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-05-14 21:05:18 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return( 
+    <div>
+        <h2>Expense Item!</h2>
+    <p>Expense Items</p>
+    <p>Food Rs 10</p>
+    <p>Petrol Rs 100</p>
+    <p>Movies Rs 200</p>
+</div>);
+}
+
+export default ExpenseItem;

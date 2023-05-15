8e3c0753de1bffb8cb2c4391b18f3ddb45c98fd9
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-05-15 20:43:13 +0530

    Added new dynamic features

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
-function ExpenseItem(){
-    return( 
-    <div>
-        <h2>Expense Item!</h2>
-    <p>Expense Items</p>
-    <p>Food Rs 10</p>
-    <p>Petrol Rs 100</p>
-    <p>Movies Rs 200</p>
-</div>);
+import "./ExpenseItem.css"
+function ExpenseItem(props) {
+  
+
+    return (
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <h2>{props.locationOfExpenditure}</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+         
+            
+        </div>
+        );
 }
 
 export default ExpenseItem;

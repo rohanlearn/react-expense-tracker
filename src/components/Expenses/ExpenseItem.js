24bc59d5dc95f715ee81993d1a4c06fba3be0317
B Rohan Rao <109647650+rohanlearn@users.noreply.github.com>
2023-05-20 16:36:13 +0530

    Added Filter feature without functionality

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,42 +5,15 @@ import ExpenseDate  from "./ExpenseDate";
 import ExpeseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
-    const [title,setTitle]=useState(props.title);
-    const [amount,setAmt]=useState(props.amount);
     
-     
-    const delbtn = (e)=>{
-        let expenses = document.querySelector(".expenses")
-        e.preventDefault();
-        
-        expenses.removeChild(e.target.parentNode) 
-        
-    }
-
-    const chngtitle =(e)=>{
-   
-        setTitle("Rohan")
-   
-    
-    
-    
-
-    }
-    const chngamt = (e)=>{
-        setAmt("100")
-
-    }
     
     return (
         <Card className="expense-item">
             
             <ExpenseDate date={props.date}/>
-            <ExpeseDetails title={title}
-        amount={amount}/>
-            
-            <button onClick={delbtn} >Delete Expense</button>
-            <button onClick={chngtitle} >Change Title</button>
-            <button onClick={chngamt} >Change Amount</button>
+            <ExpeseDetails title={props.title}
+        amount={props.amount}/>
+          
  
         </Card>
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,15 +2,22 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import { useState } from 'react';
-import { useEffect } from 'react';
+
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
-  const [expenses, setExpenses] = useState(props.items);
  
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
 
 
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {props.items.map((item) => (
   <ExpenseItem
     key={item.id} // Make sure to assign a unique key to each item
